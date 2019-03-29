1dafd192b39089770904f3c6a8b1df91e4dc2b5a
xiaonengwu <xiaonengwu@kemai.cn>
2019-03-29 17:29:13 +0800

    '修改webpack.config.js'

--- webpack.config.js
+++ webpack.config.js
@@ -94,7 +94,8 @@ module.exports = {
       ]
     }]
   },
-  devtool: '#cheap-module-eval-source-map',
+  // devtool: '#cheap-module-eval-source-map',
+  devtool: '#source-map',
   plugins: [
     new ClearWebpackPlugin(),
     new ParallelUglifyPlugin({
